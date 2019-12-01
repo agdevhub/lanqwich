1b285a0d2754dd5769bae6b46de353cd7007fb85
Adil G <adil.gurban@gmail.com>
2019-12-01 11:26:55 +0100

    Basic auth (user, pass) with passport js - backend implementation

---
+++ backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://arabicprojectdbuser:arabicprojectdbuser2019@ds147926.mlab.com:47926/arabicprojectdb",
+  secretOrKey: "secret"
+};
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+
 const learningmodules = require("./routes/api/learning-modules");
 const learningunits = require("./routes/api/learning-units");
 const questions = require("./routes/api/questions");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 // connect to database
 connectDB();
@@ -21,9 +24,16 @@ app.use("/api/learning-modules", learningmodules);
 app.use("/api/learning-units", learningunits);
 app.use("/api/questions", questions);
 
-var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')))
+// Passport middleware
+app.use(passport.initialize()); // Passport config
+require("./config/passport")(passport); // Routes
+app.use("/api/users", users);
+
+var path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT | 5000;
 
-app.listen(port, () => console.log("Express server is running on port " + port + "."));
+app.listen(port, () =>
+  console.log("Express server is running on port " + port + ".")
+);
